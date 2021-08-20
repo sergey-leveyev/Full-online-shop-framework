fe505b87922a892817d02566642a06b6d5d71c1e
unknown <sargey7@gmail.com>
2021-08-20 10:47:21 +0300

    first commit

---
+++ config.php
@@ -0,0 +1,12 @@
+<?php
+
+defined('VG_ACCES') or die('Access denied');
+
+const SITE_URL = 'http://localhost/Shop_v1/';
+const PATH = '/';
+
+const HOST = "localhost";
+const USER = 'root';
+const PASS = '';
+const DB_NAME = 'im';
+
---
+++ core/base/settings/internal_settings.php
@@ -0,0 +1,34 @@
+<?php
+
+defined('VG_ACCES') or die('Access denied');
+
+const TEMPLATE = 'templates/default/';
+const ADMIN_TEMPLATE = 'core/admin/views/';
+
+const COOKIE_VERSION = '1.0.0';
+const CRYPT_KEY = '';
+const COOKIE_TIME = 60;
+const BLOCK_TIME = 3;
+
+const QTY = 8;
+const QTY_LINKS = 3;
+
+const ADMIN_CSS_JS = [
+    'styles' => [],
+    'scripts' => []
+];
+
+use core\base\exceptions\RouteException;
+
+/**
+ * @throws RouteException
+ */
+function autoloadMainClass($class_name) {
+    $class_name = str_replace('\\', '/', $class_name);
+
+    if(!@include $class_name . '.php') {
+        throw new RouteException('Incorrect file name - ' .$class_name);
+    }
+}
+
+spl_autoload_register('autoloadMainClass');
