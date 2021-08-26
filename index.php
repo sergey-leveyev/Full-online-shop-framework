21c445dc5f364e40f64c67ff05fbb8a5829a2d99
unknown <sargey7@gmail.com>
2021-08-26 12:08:10 +0300

    260821

--- index.php
+++ index.php
@@ -11,27 +11,10 @@ require_once 'config.php';
 require_once 'core/base/settings/internal_settings.php';
 
 use core\base\exceptions\RouteException;
+use core\base\controllers\RouteController;
 
 try{
-    (new A());
+    RouteController::getInstance();
 }catch (RouteException $e){
     exit($e->getMessage());
-}
-
-class A {
-    public function __construct() {
-        (new B());
-    }
-}
-
-class B {
-    public function __construct() {
-        (new C());
-    }
-}
-
-class C {
-    public function __construct() {
-        throw new RouteException('bla bla');
-    }
 }
